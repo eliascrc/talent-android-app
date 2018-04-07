a6ccc4b31666a8fcf8d39859a14a93e50eec0761
renato2110 <renato.mainieri2110@gmail.com>
2018-04-07 01:10:58 -0600

    TAL-198 | Create SessionStorage Class
    Creation of the SessionStorage class for storing values that are required across the application lifetime. Aggregation of activity in AndroidManifest and the respective dependencies in build.gradle to use Icepick and Dagger 2.

---
+++ app/src/main/java/cr/talent/SessionStorage.java
@@ -0,0 +1,37 @@
+package cr.talent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import javax.inject.Singleton;
+import icepick.State;
+import static icepick.Icepick.restoreInstanceState;
+import static icepick.Icepick.saveInstanceState;
+
+/**
+ * This class storing values that are required across the application lifetime
+ * so they are accessible at any time they are needed.
+ *
+ * @author Renato Mainieri Sáenz.
+ */
+@Singleton
+public class SessionStorage extends AppCompatActivity {
+
+    @State String cookieValue;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        restoreInstanceState(this, savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        restoreInstanceState(this, savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveInstanceState(this, outState);
+    }
+}
