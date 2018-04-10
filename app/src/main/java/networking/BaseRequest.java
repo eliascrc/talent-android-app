8856444f43d26eba4e161b11df4ef2981e044eca
Renato Mainieri Saenz <b54076@ecci.ucr.ac.cr>
2018-04-10 16:19:28 -0600

    TAL-199 | Create Base Request Class
    Creation of the BaseRequest class.

---
+++ app/src/main/java/networking/BaseRequest.java
@@ -0,0 +1,10 @@
+package networking;
+
+/**
+ * This class makes a network request so that common configuration like retry policy is consolidated.
+ *
+ * @author Renato Mainieri Sáenz.
+ */
+
+public abstract class BaseRequest {
+}
