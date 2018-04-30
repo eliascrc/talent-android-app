8827628ccb1e749002eb3e710c3e6b085238ad60
renato2110 <renato.mainieri2110@gmail.com>
2018-04-29 21:46:59 -0600

    TAL-234 | Android | Splash Screen
    Creation of the Splash Screen for Talent! application.

--- app/src/main/java/cr/talent/SplashActivity.java
+++ app/src/main/java/cr/talent/SplashActivity.java
@@ -3,8 +3,6 @@ package cr.talent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-
 
 /**
  * This class starts the splash activity for the Talent! application. When the theme(logo) is displayed it will route the
@@ -19,15 +17,6 @@ public class SplashActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        // Stores the metrics of the actual dispositive in run time
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        // Stores the new metrics to the Talent! logo in the launch_screen
-        int width = (int) (metrics.widthPixels * 0.85);
-        int height = (int) (metrics.heightPixels * 0.40);
-
-        // TODO: assign the new metrics to the Talent! logo
-
         Intent loginActivity = new Intent(SplashActivity.this, LoginActivity.class);
         SplashActivity.this.startActivity(loginActivity);
     }
