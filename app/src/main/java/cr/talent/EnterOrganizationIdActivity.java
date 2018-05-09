4f13d324ff43abb410ce3d1ffd34eb52c191026b
Fabian Roberto Leandro <roleandroflores@hotmail.com>
2018-05-09 01:18:14 -0600

    TAL-236 Android | Sign In
    Modifications to the enter organization id activity, started working on sign in activity.

--- app/src/main/java/cr/talent/EnterOrganizationIdActivity.java
+++ app/src/main/java/cr/talent/EnterOrganizationIdActivity.java
@@ -1,7 +1,9 @@
 package cr.talent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EnterOrganizationIdActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class EnterOrganizationIdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_organization_id);
+    }
+
+    public void enterOrganizationId(View view) {
+        Intent signInActivity = new Intent(EnterOrganizationIdActivity.this, SignInActivity.class);
+        EnterOrganizationIdActivity.this.startActivity(signInActivity);
     }
 }
