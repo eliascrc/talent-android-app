2c0fa81d426395cd91228ee12dc05588397a4972
Fabian Roberto Leandro <roleandroflores@hotmail.com>
2018-05-14 10:16:42 -0600

    TAL-239 | Android | Sign In
    Changed NetworkConstants to connect to development server instead of locally.

--- app/src/main/java/networking/NetworkConstants.java
+++ app/src/main/java/networking/NetworkConstants.java
@@ -2,6 +2,6 @@ package networking;
 
 public final class NetworkConstants {
     public static final String  termsOfServiceUrl = "http://ws.talent.cr/talent-ws-0.1/ws/content/termsOfService";
-    public static final String SIGN_IN_URL = "http://10.0.2.2:8080/talent/ws/login";
-    public static final String GET_ORGANIZATION_URL = "http://10.0.2.2:8080/talent/ws/login/organization?uniqueIdentifier=";
+    public static final String SIGN_IN_URL = "http://ws.talent.cr/ws/login";
+    public static final String GET_ORGANIZATION_URL = "http://ws.talent.cr/ws/login/organization?uniqueIdentifier=";
 }
