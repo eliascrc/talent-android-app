16553b614312d00e74c36b5f8d90947a045f580d
Renato Mainieri <renato.mainieri2110@gmail.com>
2018-05-15 19:43:03 +0000

    TAL-235	| Android | Landing View
    Creation of the Landing View for Talent! application.

--- app/src/main/java/networking/NetworkConstants.java
+++ app/src/main/java/networking/NetworkConstants.java
@@ -2,5 +2,5 @@ package networking;
 
 public final class NetworkConstants {
     public static final String TERMS_OF_SERVICE_URL = "http://ws.talent.cr/ws/content/termsOfService";
-    public static final String PRIVACY_POLICY_URL = "http://ws.talent.cr/ws/content/privacy-policy";
+    public static final String PRIVACY_POLICY_URL = "http://ws.talent.cr/ws/content/privacyPolicy";
 }
